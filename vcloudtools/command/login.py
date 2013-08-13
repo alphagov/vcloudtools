4a41761bd4c55f8285826e9276fea94840a08afd
Dan Carley <dan.carley@gmail.com>
2013-08-13 15:31:14 +0100

    Add username argument to vcloud-login
    Allows a username to be passed to `vcloud-login` on the command line. Rather
    than always prompting for it.
    
    This makes it easier to use against Platform1, where the username is a UID
    and has to be suffixed with the organisation UID, which is annoying when the
    auth token expires every hour or so.

--- vcloudtools/command/login.py
+++ vcloudtools/command/login.py
@@ -10,24 +10,25 @@ from vcloudtools.api import VCloudAPIClient, envkey
 log = logging.getLogger(__name__)
 parser = ArghParser()
 
-def login_if_needed(vcloud):
+def login_if_needed(vcloud, user):
     if vcloud.logged_in:
         log.info("Already logged in")
         return
 
     print("Please log into vCloud", file=sys.stderr)
-    username = _raw_input("Username: ")
+    username = user or _raw_input("Username: ")
     password = getpass("Password: ")
 
     vcloud.login(username, password)
 
+@arg('-u', '--user', help='Username', default=None)
 def login(args):
     """
     Log into a vCloud instance, and print the resulting auth token.
     """
     c = VCloudAPIClient()
 
-    login_if_needed(c)
+    login_if_needed(c, args.user)
 
     print("export {0}='{1}'".format(envkey('auth_token'), c.token))
 
